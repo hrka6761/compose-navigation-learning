cd16469c83fd7cc0fa923d27ae28dab3d5d1d5b7
HAMIDREZA KARAMI <hrka6761@gmail.com>
2025-03-11 01:29:21 -0700

    Create MainContainer.kt

---
+++ app/src/main/java/ir/hrka/composenavigation/MainContainer.kt
@@ -0,0 +1,11 @@
+package ir.hrka.composenavigation
+
+import androidx.compose.runtime.Composable
+import ir.hrka.composenavigation.ui.theme.ComposeNavigationTheme
+
+@Composable
+fun AppContent() {
+    ComposeNavigationTheme {
+
+    }
+}
