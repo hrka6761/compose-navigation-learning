5fa409110f8cfa6af713633c1ccff05ca545e04a
HAMIDREZA KARAMI <hrka6761@gmail.com>
2025-03-15 17:08:30 +0330

    temp

--- app/src/main/java/ir/hrka/composenavigation/screens/secondary_graph/contact/Contact.kt
+++ app/src/main/java/ir/hrka/composenavigation/screens/secondary_graph/contact/Contact.kt
@@ -1,2 +1,21 @@
 package ir.hrka.composenavigation.screens.secondary_graph.contact
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import ir.hrka.composenavigation.core.utilities.SecondaryScreens.Contact
+
+@Composable
+fun ContactScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(Contact.destination)
+    }
+}
