ba8dc2aff5925a1273efc09386f287ed784c7988
HAMIDREZA <hrka6761@gmail.com>
2025-03-20 15:34:09 +0330

    Create DataStoreManager.kt
    (cherry picked from commit 56e48cbe1d23d9a7324da9fb72210d25b2973929)

---
+++ app/src/main/java/ir/hrka/composenavigation/core/utilities/DataStoreManager.kt
@@ -0,0 +1,23 @@
+package ir.hrka.composenavigation.core.utilities
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class DataStoreManager @Inject constructor(@ApplicationContext private val context: Context) {
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_prefs")
+
+
+    suspend fun <T> saveData(preferencesKey: Preferences.Key<T>, value: T) =
+        context.dataStore.edit { preferences -> preferences[preferencesKey] = value }
+
+    fun <T> readData(preferencesKey: Preferences.Key<T>): Flow<T?> =
+        context.dataStore.data.map { preferences -> preferences[preferencesKey] }
+}
