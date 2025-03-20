6c923c2386d101a21122c9583d64fd96ecca10c9
HAMIDREZA <hrka6761@gmail.com>
2025-03-20 17:08:33 +0330

    Implement base of hilt setting
    (cherry picked from commit 1200ddfbfbec41a9fdba6605488d85794c08fc61)

--- app/src/main/java/ir/hrka/composenavigation/MainActivity.kt
+++ app/src/main/java/ir/hrka/composenavigation/MainActivity.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import ir.hrka.composenavigation.core.Constants.TAG
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
